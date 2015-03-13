2ef3f064a2188c1726cc4669f618dad71a5eeaa8
gmammolo <gmammolo@gmail.com>
2015-03-13 10:53:57 +0100

    Aggiunto  BinTree in C

---
+++ src/c/Algoritmi/BenTree.h
@@ -0,0 +1,4 @@
+#ifndef BENTREE_H
+#define BENTREE_H
+
+#endif // BENTREE_H
---
+++ src/c/Algoritmi/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <bintree.h>
+
+int main(void)
+{
+    //BinTree tree = newLeaf(1);
+    printf("Hello World!\n");
+    return 0;
+}
+
